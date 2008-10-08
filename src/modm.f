b813f12713555835b7d79fd4c7cd11e39118dc86
vpayne <vpayne>
2008-10-08 16:10:09 +0000

    Updated copyright information to 2008 for relevant source files.

--- src/modm.f
+++ src/modm.f
@@ -6,6 +6,18 @@ C     created:	        $Date$
       SUBROUTINE MODM(ICP,NWN,WN,dvset,NLAY,P,T,
      &                 NMOL,WKL,WBRODL,
      &           SCLCPL,SCLHW,Y0RES,HFILE,ICNTNM,ixsect,ISPD)
+C
+C  --------------------------------------------------------------------------
+C |                                                                          |
+C |  Copyright 2002 - 2008, Atmospheric & Environmental Research, Inc. (AER).|
+C |  This software may be used, copied, or redistributed as long as it is    |
+C |  not sold and this copyright notice is reproduced on each copy made.     |
+C |  This model is provided as is without any express or implied warranties. |
+C |                       (http://www.rtweb.aer.com/)                        |
+C |                                                                          |
+C  --------------------------------------------------------------------------
+C
+C        
 C-------------------------------------------------------------------------------
 C
 C     PROGRAM:  MODM
--- src/monortm_sub.f
+++ src/monortm_sub.f
@@ -5,6 +5,19 @@ C     created:	        $Date$
 
 	SUBROUTINE RTM(IOUT,IRT,NWN,WN,NLAY,T,TZ,
      &    TMPSFC,  RUP,TRTOT,RDN,REFLC,EMISS,RAD,TB,IDU)
+
+C
+C  --------------------------------------------------------------------------
+C |                                                                          |
+C |  Copyright 2002 - 2008, Atmospheric & Environmental Research, Inc. (AER).|
+C |  This software may be used, copied, or redistributed as long as it is    |
+C |  not sold and this copyright notice is reproduced on each copy made.     |
+C |  This model is provided as is without any express or implied warranties. |
+C |                       (http://www.rtweb.aer.com/)                        |
+C |                                                                          |
+C  --------------------------------------------------------------------------
+C
+C        
 C-------------------------------------------------------------------------------
 C
 C     PROGRAM:  RTM
