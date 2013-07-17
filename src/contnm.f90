4090261d9afdc682047566104060022b43f3d3f6
alipton <alipton>
2013-07-17 06:37:43 +0000

    Implement new method for accessing constants.

--- src/contnm.f90
+++ src/contnm.f90
@@ -24,7 +24,7 @@
       SUBROUTINE CONTNM(JRAD) 
 !                                                                       
       Use lblparams, ONLY: n_absrb, ipts, ipts2
-      USE phys_consts, ONLY: radcn2
+      USE PhysConstants, ONLY: getPhysConst
       IMPLICIT REAL*8           (V) 
 !                                                                       
 !     SUBROUTINE CONTNM CONTAINS THE CONTINUUM DATA                     
@@ -73,6 +73,7 @@
       DIMENSION CCH0(5150),CCH1(5150),CCH2(5150) 
 !                                                                       
       REAL ABSBSV(n_absrb) 
+      REAL RADCN2
 !                                                                       
       CHARACTER*18 HNAMCNT,HVRCNT 
 !                                                                       
@@ -223,6 +224,7 @@
       HVRCNT = '$Revision: 16421 $'
 !                                                                       
       RHOAVE = (PAVE/P0)*(T0/TAVE)                                      
+      CALL getPhysConst(RADCN2=RADCN2)
       XKT = TAVE/RADCN2                                                 
                                                                         
 !     the amagat value is used for the broadenening component for a numb
